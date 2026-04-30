8d3d75a577aad32c2ce381c4f1b8c593d15cddcf
njokomcourage-create <njokomcourage@gmail.com>
2026-04-30 11:54:42 +0100

    Update bronze_config.py
    This is a dictionary that contains the source name,file path,and the destination name of the csv files which we are reading to write in the bronze layer

--- code/demo/code/bronze/bronze_config.py
+++ code/demo/code/bronze/bronze_config.py
@@ -1,37 +1,38 @@
 BASE_PATH = '/Volumes/workspace/bronze/raw_files'
 INGESTION_CONFIG = [
-  (
+  {
     'source': 'crm',
     'path':f'{BASE_PATH}/source_crm/cust_info.csv',
-    'table':crm_cust_info_raw'
-  ),
+    'table':'crm_cust_info_raw'
+  },
 
-  (
+  {
     'source': 'crm',
     'path':f'{BASE_PATH}/source_crm/prd_info.csv',
-    'table':crm_prd_info_raw'
-  ),
+    'table':'crm_prd_info_raw'
+  },
 
-  (
+  {
     'source': 'crm',
     'path':f'{BASE_PATH}/source_crm/sales_details.csv',
-    'table':crm_sales_details_raw'
-  ),
+    'table':'crm_sales_details_raw'
+  },
 
-  (
+  {
     'source': 'erp',
     'path':f'{BASE_PATH}/source_erp/CUST_AZ12.csv',
-    'table':erp_CUST_AZ12_raw'
-  ),
+    'table':'erp_CUST_AZ12_raw'
+  },
 
-  (
+  {
     'source': 'erp',
     'path':f'{BASE_PATH}/source_erp/LOC_A101.csv',
-    'table':erp_LOC_A101_raw'
-  ),
+    'table':'erp_LOC_A101_raw'
+  },
 
-  (
+  {
     'source': 'erp',
     'path':f'{BASE_PATH}/source_erp/PX_CAT_G1V2.csv',
-    'table':erp_PX_CAT_G1V2_raw'
-  )
+    'table':'erp_PX_CAT_G1V2_raw'
+  }
+]
